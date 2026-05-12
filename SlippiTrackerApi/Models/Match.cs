8798b7c7dec427769c857eaa7c50387c730000be
RileighLuvsCatz <rdettlingburki@gmail.com>
2026-05-11 21:46:11 -0500

    update match & add StockEvent models

--- SlippiTrackerApi/Models/Match.cs
+++ SlippiTrackerApi/Models/Match.cs
@@ -3,5 +3,7 @@ class Match {
     DateTime Date { get; set; }
     string Stage { get; set; }
     int DurationSeconds { get; set; }
+    bool Completed { get; set; }
     List<Player> Players { get; set; }
+    List<StockEvent> StockEvents { get; set; }
 }
---
+++ SlippiTrackerApi/Models/StockEvent.cs
@@ -0,0 +1,6 @@
+class StockEvent {
+    int PlayerPort { get; set; }
+    int StocksRemaining { get; set; }
+    double DamageAtDeath { get; set; }
+    int TimeSeconds { get; set; }
+}
